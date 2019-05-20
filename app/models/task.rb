f6f8a7c590849d66146564c4dc409574023dab30
ryota tanabe <teni0abe@gmail.com>
2019-05-20 17:15:16 +0900

    [update]Taskモデルにバリデーションを追加しエラメッセージが出るようにした

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { maximum: 140 }
+  validates :priority, presence: true
+  validates :status, presence: true
+  validates :deadline, presence: true
 end
