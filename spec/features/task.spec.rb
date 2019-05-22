c431882fbbc04774bf34ff189a19b2eb79741666
ryota tanabe <teni0abe@gmail.com>
2019-05-22 16:20:16 +0900

    [update]終了期限でソート可能にする実装とそのテストの実装

--- spec/features/task.spec.rb
+++ spec/features/task.spec.rb
@@ -1,9 +1,8 @@
 # このrequireで、Capybaraなどの、Feature Specに必要な機能を使用可能な状態にしています
 require 'rails_helper'
 
-# このRSpec.featureの右側に、「タスク管理機能」のように、テスト項目の名称を書きます（do ~ endでグループ化されています）
 RSpec.feature "タスク管理機能", type: :feature do
-  let(:task_a) { Task.create!(title: 'こんにちは', content: '世界') }
+  let(:task_a) { Task.create!(title: 'こんにちは', content: '世界', priority: '最高', status: '着手中', deadline: '100') }
   background do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
@@ -16,40 +15,34 @@ RSpec.feature "タスク管理機能", type: :feature do
   end
 
   scenario "タスク作成のテスト" do
-  # new_task_pathにvisitする（タスク登録ページに遷移する）
-  # 1.ここにnew_task_pathにvisitする処理を書く
     visit new_task_path
-  # 「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄に
-  # タスクのタイトルと内容をそれぞれfill_in（入力）する
-  # 2.ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-  # 3.ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
     fill_in 'task_title', with: 'なごみの湯'
     fill_in 'task_content', with: '1人でもくもく'
-
-  # 「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）
-  # 4.「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+    fill_in 'task_priority', with: '高'
+    fill_in 'task_status', with: '未着手'
+    fill_in 'task_deadline', with: '7'
     click_on 'タスクを登録する'
-
-  # clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
-  # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
-  # 5.タスク詳細ページに、テストコードで作成したはずのデータ（記述）がhave_contentされているか（含まれているか）を確認（期待）するコードを書く
     expect(page).to have_content 'なごみの湯'
     expect(page).to have_content '1人でもくもく'
+    expect(page).to have_content '高'
+    expect(page).to have_content '未着手'
+    expect(page).to have_content '7'
   end
 
   scenario "タスク詳細のテスト" do
     # 6行目でセットしてある初期値を使ってテストを作成
     visit task_path(task_a)
-    expect(page).to have_content "タスク詳細画面\nタスク名 タスク詳細 こんにちは 世界\nタスク一覧へ戻る"
-
-    # 以下のようなやり方でも可能
-    # task = Task.create!(title: 'test1',content: 'test5555')
-    # visit task_path(task.id)
-    # expect(page).to have_content 'test5555'
+    expect(page).to have_content "こんにちは 世界 最高 着手中 100"
   end
 
   scenario "タスクが作成日時の降順に並んでいるかのテスト" do
     visit tasks_path
-    expect(Task.order("created_at DESC").each
+    expect(Task.order("created_at DESC").each)
+  end
+
+  scenario "タスクが終了期限順に並んでいるかのテスト" do
+    visit tasks_path
+    click_on "終了期限でソートする"
+    expect(Task.order("deadline ASC").each)
   end
 end
