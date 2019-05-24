de28d4bcf21918fc7edac4c8b97cfe06fabd7441
ryota tanabe <teni0abe@gmail.com>
2019-05-24 17:37:43 +0900

    [update]tasksテーブルのstatusカラムのデータ型をintegerへ変更(gemを使うため)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_20_072606) do
+ActiveRecord::Schema.define(version: 2019_05_24_080824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_05_20_072606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "priority", null: false
-    t.string "status", null: false
+    t.integer "status", null: false
     t.integer "deadline", null: false
     t.string "label"
   end
