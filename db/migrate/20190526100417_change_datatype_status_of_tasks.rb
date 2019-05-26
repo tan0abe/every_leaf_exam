b8737345b81589e4ccbb66cb752b8095966d940f
ryota tanabe <teni0abe@gmail.com>
2019-05-26 19:06:22 +0900

    [update]tasksテーブルのstatusカラムのデータ型をstringに変更(gemを使用しなくなったため)

--- db/migrate/20190526100417_change_datatype_status_of_tasks.rb
+++ db/migrate/20190526100417_change_datatype_status_of_tasks.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeStatusOfTasks < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :status, 'integer USING CAST(status AS integer)'
+    change_column :tasks, :status, :string
   end
 end
