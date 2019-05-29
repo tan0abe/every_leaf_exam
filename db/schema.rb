e37768a346c333fc5e2c671de5e0c79818720b9c
ryota tanabe <teni0abe@gmail.com>
2019-05-29 15:21:20 +0900

    [update]tasksテーブルのpriorityカラムのデータ型をintegerに変更(それに伴いカラムindexが削除された)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_064347) do
+ActiveRecord::Schema.define(version: 2019_05_28_081349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,10 @@ ActiveRecord::Schema.define(version: 2019_05_28_064347) do
     t.string "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "priority", null: false
     t.string "status", default: "", null: false
     t.string "label"
     t.date "deadline", null: false
-    t.index ["title", "status", "priority"], name: "index_tasks_on_title_and_status_and_priority"
+    t.integer "priority", null: false
   end
 
 end
