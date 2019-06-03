fbc43e2c108f7b924ff617bd46037e34b261813e
ryota tanabe <teni0abe@gmail.com>
2019-06-03 18:12:25 +0900

    [update]usersテーブルのname,email,password_digestカラムにnot null制約を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_02_071002) do
+ActiveRecord::Schema.define(version: 2019_06_03_083613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2019_06_02_071002) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
