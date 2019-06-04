fc7c17d1ccb4bcdb6be16b6494b886b4bc088152
ryota tanabe <teni0abe@gmail.com>
2019-06-04 16:15:15 +0900

    [update]belongs_toとhas_manyを使用しUserモデルとTaskモデルのアソシエーションを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 140 }
   validates :priority, presence: true
