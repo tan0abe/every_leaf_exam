f5e2cb3510134430a7b83c921a562c8bc0bbc409
ryota tanabe <teni0abe@gmail.com>
2019-06-04 15:07:19 +0900

    [update]add_referenceを使用しUserモデルとTaskモデルの関連付けを行った

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_031247) do
+ActiveRecord::Schema.define(version: 2019_06_04_055919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_06_04_031247) do
     t.string "label"
     t.date "deadline", null: false
     t.integer "priority", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_06_04_031247) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
