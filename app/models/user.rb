a67b85d88a2456501c2b80ae69cf6e5ce753b82d
ryota tanabe <teni0abe@gmail.com>
2019-06-04 11:49:07 +0900

    [add]Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 100 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+            uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_validation { email.downcase! }  #メールアドレスを小文字へ変換
+  has_secure_password  #パスワードをハッシュ化
 end
