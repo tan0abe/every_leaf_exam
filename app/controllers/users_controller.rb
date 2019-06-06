efa085deecbca1fc846572ab59c9e21341ede415
ryota tanabe <teni0abe@gmail.com>
2019-06-06 15:33:21 +0900

    [update]アカウント作成完了後に自動的にログインするように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
       flash[:notice] = "ユーザー登録しました"
     else
