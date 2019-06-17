491ce63485ad22517e00ef45d8981c789dea88fd
ryota tanabe <teni0abe@gmail.com>
2019-06-17 14:25:03 +0900

    [add]管理者のみユーザー管理画面が見れるようにリンクの設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,9 +5,29 @@ class Admin::UsersController < ApplicationController
   def new
   end
 
+  def create
+
+  end
+
   def show
   end
 
   def edit
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def admin_required
+    unless current_user.admin?
+      redirect_to root_path, notice: "権限がありません"
+    end
   end
 end
