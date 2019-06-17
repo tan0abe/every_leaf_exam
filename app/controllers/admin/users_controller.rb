70b0a4ae6bb27754256cc8c5d53451bf7695647f
ryota tanabe <teni0abe@gmail.com>
2019-06-17 16:50:22 +0900

    [add]管理者からのユーザー登録機能とビューの作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,10 +3,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path(@user.id), notice: "ユーザー登録が完了しました"
+    else
+      render "new"
+    end
   end
 
   def show
@@ -24,6 +30,14 @@ class Admin::UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+  end
 
   def admin_required
     unless current_user.admin?
