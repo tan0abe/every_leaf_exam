89cdf4a7de94e4e8386c2c1b97eb29131fe62941
ryota tanabe <teni0abe@gmail.com>
2019-06-17 11:33:20 +0900

    [add]admin/users_controllerとビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/index'
+    get 'users/new'
+    get 'users/show'
+    get 'users/edit'
+  end
   root 'tasks#index'
   resources :tasks
   resources :users, only: [:new, :show, :create]
