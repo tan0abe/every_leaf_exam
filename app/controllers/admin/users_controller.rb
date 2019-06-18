2aa7ce7949e79f3ec72a81ae27a1d328dfcba322
ryota tanabe <teni0abe@gmail.com>
2019-06-18 12:22:28 +0900

    [update]管理者が自分のアカウントを削除できないように実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,12 @@
 class Admin::UsersController < ApplicationController
   before_action :admin_required
+  before_action :prohibited_admin_destroy, only: [:destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   PER = 10
 
   def index
-    @users = User.page(params[:page]).per(PER)
+    @users = User.all.order(admin: :desc).page(params[:page]).per(PER)
   end
 
   def new
@@ -51,6 +52,13 @@ class Admin::UsersController < ApplicationController
   def admin_required
     unless current_user.admin?
       redirect_to root_path, notice: "権限がありません"
+    end
+  end
+
+  #管理者自身が自分のアカウントを削除できないように制御
+  def prohibited_admin_destroy
+    if current_user.admin? && current_user === @user
+      redirect_to admin_users_path , notice: "管理者自身の削除はできません"
     end
   end
 end
